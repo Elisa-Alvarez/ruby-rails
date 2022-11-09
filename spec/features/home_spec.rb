b0be981262ac1267dd3c14cceec42814c1fbda04
Elisa Alvarez <elisaalvarez211@gmail.com>
2022-11-08 23:48:49 -0800

    setup tests and pg

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,7 @@
+require 'ralis_helper'
+
+Rspec.describe 'Home Features' do
+  it 'Displays The Home Page' do
+  visit('/')  
+end
+end
---
+++ spec/tests_spec.rb
@@ -0,0 +1,7 @@
+require "spec_helper.rb"
+describe "Tests" do 
+   it "should return 2 + 2"do   
+   sum = 2+2
+   expect(sum).to eq(4)
+   end
+end
